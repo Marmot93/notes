04679a448f2961a5a9a8f55c0c880688085856b2
mou.wenfeng <mou.wenfeng@xiaoxiang.tech>
2022-02-28 14:57:40 +0800

    kafka

--- python/kafka_consumer.py
+++ python/kafka_consumer.py
@@ -24,14 +24,18 @@ def run():
     settings: dict = {}
     kwargs: dict = {}
     topics: list = []
-    c = Consumer(settings, logger=get_logger(), **kwargs)
+    logger = get_logger()
+    c = Consumer(settings, logger=logger, **kwargs)
     # Subscribe to topics
     c.subscribe(topics, on_assign=print_assignment)
     while True:
+        logger.info("start")
         msg = c.poll(timeout=1.0)
         if msg is None:
+            logger.info("get None")
             continue
         if msg.error():
+            logger.error("KafkaException")
             raise KafkaException(msg.error())
         else:
             # Proper message
